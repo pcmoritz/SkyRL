bf8d9d5b0fb94c1867381119ebbce83758637348
Philipp Moritz <pcmoritz@gmail.com>
2026-01-12 20:37:44 -0800

    debug

--- skyrl-tx/tx/layers/util.py
+++ skyrl-tx/tx/layers/util.py
@@ -32,6 +32,7 @@ def ragged_dot(
     all_use_fast = lax.pmin(can_use_fast, axis_name="ep") > 0
 
     def fast_path(_):
+        jax.debug.print("fast_path: offset={}, num_valid={}, local_capacity={}, m={}", offset, num_valid, local_capacity, m)
         lhs_padded = jnp.pad(lhs, ((0, local_capacity), (0, 0)))
         lhs_slice = lax.dynamic_slice(lhs_padded, (shard_start, 0), (local_capacity, k))
         adjusted = local_sizes.at[-1].add(local_capacity - num_valid)
@@ -40,6 +41,7 @@ def ragged_dot(
         return lax.dynamic_update_slice(jnp.zeros((m, rhs.shape[-1]), result.dtype), result, (shard_start, 0))
 
     def full_path(_):
+        jax.debug.print("full_path: offset={}, num_valid={}, local_capacity={}, m={}", offset, num_valid, local_capacity, m)
         adjusted = local_sizes.at[0].add(shard_start).at[-1].add(m - shard_end)
         result = lax.ragged_dot(lhs, rhs, adjusted, precision=precision, preferred_element_type=preferred_element_type)
         mask = (jnp.arange(m) >= shard_start) & (jnp.arange(m) < shard_end)
