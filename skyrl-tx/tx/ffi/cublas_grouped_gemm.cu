ecf5f90beeb589df1ad3c6e59a404be137b9678e
Philipp Moritz <pcmoritz@gmail.com>
2026-01-16 23:26:42 -0800

    update

--- skyrl-tx/tx/ffi/cublas_grouped_gemm.cu
+++ skyrl-tx/tx/ffi/cublas_grouped_gemm.cu
@@ -188,6 +188,7 @@ xla::ffi::Error CublasGroupedGemmImpl(
   std::vector<int> ldc_array;
   std::vector<cublasOperation_t> transa_array;
   std::vector<cublasOperation_t> transb_array;
+  std::vector<int> group_size_array;
   std::vector<const void*> a_array;
   std::vector<const void*> b_array;
   std::vector<void*> c_array;
@@ -219,6 +220,7 @@ xla::ffi::Error CublasGroupedGemmImpl(
     ldc_array.push_back(static_cast<int>(n));
     transa_array.push_back(CUBLAS_OP_N);
     transb_array.push_back(CUBLAS_OP_N);
+    group_size_array.push_back(1);
     a_array.push_back(a_ptr);
     b_array.push_back(b_ptr);
     c_array.push_back(c_ptr);
@@ -261,7 +263,7 @@ xla::ffi::Error CublasGroupedGemmImpl(
       dtype_info.data_type, lda_array.data(), b_array.data(),
       dtype_info.data_type, ldb_array.data(), beta, c_array.data(),
       dtype_info.data_type, ldc_array.data(), group_count,
-      dtype_info.compute_type, CUBLAS_GEMM_DEFAULT);
+      group_size_array.data(), dtype_info.compute_type);
   if (status != CUBLAS_STATUS_SUCCESS) {
     return CublasError(status, "cublasGemmGroupedBatchedEx failed");
   }
